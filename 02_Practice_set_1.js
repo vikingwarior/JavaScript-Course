e2f4e8b3734359c95bd75ec7830d086f22c21b6a
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-11-13 02:18:33 +0530

    new file:   02_Practice_set_1.js

---
+++ 02_Practice_set_1.js
@@ -0,0 +1,15 @@
+// number null boolean bigInt string symbol undefined
+let s = ""
+t = 3
+console.log(s+t)
+console.log(typeof s)
+
+const r = {
+    name : 'Harsh',
+    age : 9
+}
+
+// r = 3  // not possible(cannot alter a constant after declaration)
+r['class'] = 3
+console.log(r)
+
