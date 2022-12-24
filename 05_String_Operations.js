35f6931652e370f1ee8796750246e09c59aa22e8
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-25 02:36:08 +0530

    modified:   05_String_Operations.js

--- 05_String_Operations.js
+++ 05_String_Operations.js
@@ -6,3 +6,17 @@ let surname = 'Paradkar'
 //Variables can directly inserted into string using template literals if the string is made using ``
 //using template literals to manipulate string is called string interpolation
 console.log(`${name} ${surname}`)
+
+//Escape Sequences
+
+// \ followed by ` or' or "" is used to print any of these 3 as strings
+console.log(`learning something \`new`)
+console.log('learning something \'new')
+console.log("learning something \"new")
+
+// \t is used to add tabspacing to text
+console.log(`learning something \tnew`)
+
+// words  followed by \r are used to replace the leading n words
+console.log(`learning something \rnew`)
+
