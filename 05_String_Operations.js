b700547d655835d732370a38c25f1f24f77cd9d0
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-25 02:26:26 +0530

    new file:   05_String_Operations.js 	modified:   footnotes.txt

---
+++ 05_String_Operations.js
@@ -0,0 +1,8 @@
+// Strings in JavaScript can also be created using `` along with '' and ""
+// Char from strings can be sliced using indexes
+let name = `Harsh`
+let surname = 'Paradkar'
+
+//Variables can directly inserted into string using template literals if the string is made using ``
+//using template literals to manipulate string is called string interpolation
+console.log(`${name} ${surname}`)
