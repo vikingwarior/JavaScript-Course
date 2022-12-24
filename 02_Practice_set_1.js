1ebc587d1f7a5e1b16608fbb31a8a2411f6ca79e
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-25 01:44:10 +0530

    modified:   02_Practice_set_1.js

--- 02_Practice_set_1.js
+++ 02_Practice_set_1.js
@@ -1,9 +1,5 @@
 // number null boolean bigInt string symbol undefined
-/*
-    Avoid using var keyword as it is a legacy keyword for the following reasons:
-    - A variable declared with var keyword can be redeclared in the same scope.
-    - Any changes to the value of variable created with var at any level will change the value of that variable globally even if the redeclaration was done in another scope.
-*/
+
 let s = ""
 t = 3
 console.log(s+t)
