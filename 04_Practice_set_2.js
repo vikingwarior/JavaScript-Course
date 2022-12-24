9822c8b5480f554a47255b8575157f5bc2dc2b77
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-25 02:04:21 +0530

    modified:   04_Practice_set_2.js

--- 04_Practice_set_2.js
+++ 04_Practice_set_2.js
@@ -1,16 +1,23 @@
-const marks = {
-    harry : 98,
-    rohan : 70,
-    aakash : 7
-};
+// const marks = {
+//     harry : 98,
+//     rohan : 70,
+//     aakash : 7
+// };
 
-//traditional approach
-for (let i = 0; i < Object.keys(marks).length; i++) {
-   console.log(marks[Object.keys(marks)[i]])
+// //traditional approach
+// for (let i = 0; i < Object.keys(marks).length; i++) {
+//    console.log(marks[Object.keys(marks)[i]])
+// }
+
+// //ES6 revamped approach
+// for(let key in marks){
+//     console.log(marks[key])
+// }
+
+// Function to calculate average of numbers
+
+let avg = (n1, n2, n3) =>{
+    return((n1+n2+n3)/3)
 }
 
-//ES6 revamped approach
-for(let key in marks){
-    console.log(marks[key])
-}
-
+console.log(avg(1,2,3))
