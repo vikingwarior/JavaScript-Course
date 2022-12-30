3267830b9edbce997e4a0427e608afbeb6336839
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2022-12-31 01:01:25 +0530

    modified:   Harsh/14_DOM/script.js

--- Harsh/14_DOM/script.js
+++ Harsh/14_DOM/script.js
@@ -1,4 +1,6 @@
 //To modify any tag:
 // document.${nameOfTheTag}
-document.title = 'F'
+// document.title = 'F'
 
+//To get all the child nodes of a particular element
+console.log(document.body.childNodes)
