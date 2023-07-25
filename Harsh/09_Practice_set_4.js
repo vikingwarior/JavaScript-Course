0926aa5176ce01aabe6976244b38190d9eef62d0
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2023-07-26 01:55:29 +0530

    modified:   Harsh/09_Practice_set_4.js

--- Harsh/09_Practice_set_4.js
+++ Harsh/09_Practice_set_4.js
@@ -1,21 +1,14 @@
-//Storing user input into an array
-// const prompt = require("prompt-sync")();
-// let nums = []
-// nums.push(parseInt(prompt(`Enter a number:`)))
-// console.log(nums)
+//Take input from user and add it to an array
+const prompt = require("prompt-sync")();
+let nums = [];
+while (true) {
+    let input = prompt("Enter a number to add to the array: ");
+    if (input == '0') {
+        break;
+    }
+    nums.push(Number(input));
+}
+console.log(`Array: ${nums}`);
 
-//Storing Squares of numbers using Map
-n = [1, 2, 3, 4, 5]
-sq = n.map((num) => {
-    return num*num
-})
-
-console.log(sq)
-
-//Calculate Factorial of array number using reduce
-
-let nums = [1,2,3,4,5]
-n = nums.reduce((n1, n2) => {
-    return n1*n2
-})
-console.log(n)
+//Filtering the numbers divisible by 10
+console.log(`Number divisible by 10: ${nums.filter((value) => {return value%10 === 0})}`);
