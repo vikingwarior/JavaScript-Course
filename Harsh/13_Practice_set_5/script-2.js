97c9ad22ee4edd589b62133aca6da2ea4e92cd15
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-08-02 15:54:59 +0530

    modified:   Harsh/13_Practice_set_5/script-2.js

---
+++ Harsh/13_Practice_set_5/script-2.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt(`Enter a number:`));
+
+if (num > 4){
+    // Can be used to alter the URL of a page and redirect it
+    window.location = "https://github.com/vikingwarior" // or // location.href = "https://github.com/vikingwarior" can also be used
+}
+
+document.body.style.backgroundColor = prompt(`Enter a color:`);
