a0d9e0cb66d2c614465c24544d6fc301a82edb16
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-08-02 15:34:35 +0530

    Changes to be committed: 	modified:   Harsh/13_Practice_set_5/index.html 	new file:   Harsh/13_Practice_set_5/script-2.js 	modified:   Harsh/13_Practice_set_5/script.js 	modified:   Harsh/14_DOM/script.js

--- Harsh/13_Practice_set_5/script.js
+++ Harsh/13_Practice_set_5/script.js
@@ -1,23 +1,19 @@
-let age = parseInt(prompt('Enter your age:'))
-
-if (age > 17) {
-  alert('You can drive!')
-}
-
-else {
-  alert('You cannot drive!')
-}
-if(age < 0){
-  console.error('lol negative age.')
-} 
-
-while (confirm('Do you want to retry?')) {
-  age = parseInt(prompt('Enter your age:'))
-
+checkAge = () =>{
+  let age = parseInt(prompt('Enter your age:'))
   if (age > 17) {
     alert('You can drive!')
   }
+  
   else {
     alert('You cannot drive!')
   }
+  if(age < 0){
+    console.error('lol negative age.')
+  } 
+}
+
+checkAge();
+
+while (confirm('Do you want to retry?')) {
+  checkAge();
 }
--- Harsh/14_DOM/script.js
+++ Harsh/14_DOM/script.js
@@ -1,8 +1,13 @@
-//To modify any tag:
+// Window is the global object of JS i.e. all other objects and methods of JS are stored in a window
+// DOM -> Document Object Model is the javascript object of the current entire webpage
+
+// To modify any tag:
 // document.${nameOfTheTag}
-// document.title = 'F'
+document.title = 'F'
 
 //To get all the child nodes of a particular element
-// console.log(document.body.childNodes)
+console.log(document.body.childNodes)
 
-//$0 -> Used to refer the curresnt selected element
+// $0 -> Used to refer the curresnt selected element
+
+// BOM -> Browser object model additional utilities provided by browser
