0558733fdfc6ad2cf8f080654e04413cd1ff4981
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-08-05 01:39:05 +0530

    Changes to be committed: 	new file:   Harsh/21-chapter-7-Practice-set/index.html 	new file:   Harsh/21-chapter-7-Practice-set/script.js 	new file:   Harsh/21-chapter-7-Practice-set/style.css

---
+++ Harsh/21-chapter-7-Practice-set/script.js
@@ -0,0 +1,17 @@
+//Q1
+let firstChildOfNavBar = document.body.getElementsByTagName("ul")[0].firstElementChild;
+console.log(firstChildOfNavBar);
+firstChildOfNavBar.style.color = "red";
+
+//Q3
+let navBarList = document.body.getElementsByTagName("ul")[0];
+navBarList.firstElementChild.style.color = "green";
+navBarList.lastElementChild.style.color = "green";
+
+//Q4
+let liElementTags = document.getElementsByTagName("li");
+// console.log(liElementTags);
+for (let element of liElementTags) {
+    element.style.backgroundColor = "cyan";
+}
+
