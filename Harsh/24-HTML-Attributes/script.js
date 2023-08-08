0c8b813967182b27f326f9b17098fa40d7ae2d75
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-08-09 00:28:35 +0530

    modified:   Harsh/24-HTML-Attributes/index.html 	modified:   Harsh/24-HTML-Attributes/script.js

--- Harsh/24-HTML-Attributes/script.js
+++ Harsh/24-HTML-Attributes/script.js
@@ -1,17 +1,41 @@
-let elem = document.getElementById("outer");
+let elem1 = document.getElementById("outer");
 
 // .hasAttribute -> Specifies whether a given element has the specified attribute like a class, id, style, script, href etc.
-console.log(elem.hasAttribute("class"));
-console.log(elem.hasAttribute("href"));
+console.log(elem1.hasAttribute("class"));
+console.log(elem1.hasAttribute("href"));
 
-elem = document.getElementsByClassName("inner")[0];
+let elem2 = document.getElementsByClassName("inner")[0];
 
 // .setAttribute -> Used to modify/add the attribute and it's value
-elem.setAttribute("style", "color: green;");
-elem.setAttribute("hidden", "true");
+elem2.setAttribute("style", "color: green;");
+elem2.setAttribute("hidden", "true");
 
 // .removeAttribute -> can be used to remove attributes from the selected elements
-elem.removeAttribute("hidden");
+elem2.removeAttribute("hidden");
 
 // .attributes -> Returns a NamedNodeMap of all the valid attrubutes that are set for a given element
-console.log(elem.attributes);
+console.log(elem2.attributes);
+
+/*
+    data-[attributeName] can be added in HTML to create attribute variables and they can be fetched in JS using:
+
+    [elementName].dataset -> Returns a Map of all the custom data variables and their corresponding values
+    [elementName].dataset.[attributeName] -> to fetch value
+*/
+
+console.log(elem1.dataset);
+console.log(elem1.dataset.name);
+
+// Trial of projects
+// console.log()
+// let insertIfTrue = () => {
+//     let Name =  prompt("Enter Validation Value");
+//     if (elem1.dataset.name === Name) {
+//         elem2.innerHTML += ` Congratulations ${Name} for the correct guess`
+//     }
+//     else{
+//         elem2.innerHTML = ` LOL Noob`
+//     }
+// }
+
+// insertIfTrue();
