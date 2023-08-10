0c3ddf4de500cc7b042d926e7fcf6ff4bfe027e6
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-08-11 00:54:47 +0530

    modified:   Harsh/25-HTML-insertion-methods/script.js

--- Harsh/25-HTML-insertion-methods/script.js
+++ Harsh/25-HTML-insertion-methods/script.js
@@ -8,14 +8,23 @@ let elem = document.getElementById("first");
 console.log(elem);
 elem.innerHTML = `Inserting using elem.innerHTML plain text`;
 
-//For inserting large no of methods with iterative changes we can use append/prepend child method
+//Second method -> For inserting large no of methods with iterative changes we can first create the methods and then  use append/prepend child 
 
 //Create a new element
+// let num = parseInt(prompt("Enter a number:"))
 
-let num = parseInt(prompt("Enter a number:"))
+
 let a = document.createElement("div");
-for (let i = 0; i < num; i++) {
-    a.innerHTML = a.innerHTML + `LOL ${i}`;
-    console.log(a);
-    elem.appendChild(a);    
-}
+a.innerHTML = `<i>inserting using element creation using append child.</i>`;
+/*
+    In-Container methods -> These methods perform all the operations within the specified container/element. i.e. The scope of thes methods will be limited to inside the container in which they are used on. Methods ->
+    -> element.append
+    -> element.prepend 
+*/
+// element.append -> Inserts at the end of the last child node of container i.e. after the last child of the element of the element container has been closed.
+elem.append(a);
+
+// element.prepend -> Inserts at the start of the container i.e. before the first child.
+elem.prepend(a);
+
+
