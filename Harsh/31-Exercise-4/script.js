2c5a5be97179f3eedc54ce616b23b6cc7a725bb9
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2023-10-03 02:20:44 +0530

    Added Stopwatch functionality

--- Harsh/31-Exercise-4/script.js
+++ Harsh/31-Exercise-4/script.js
@@ -1,14 +1,35 @@
-const weekDays = ["Sunday", "Mon", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-let date = document.getElementById("date"); 
-let time = document.getElementById("time");
+let watchFace = document.getElementById("watchFace");
+let watchButton = document.getElementById("watch");
+let stopwatchButton = document.getElementById("stopwatch");
+let timerButton = document.getElementById("timer");
 
-const renderDefaultClock = () => {
+// This functions creates parameters for a basic clock
+const renderClock = (clockFace) => {
+    clockFace.innerHTML = "";
     let DateObject = new Date();
+    let date = document.createElement("div");
+    let time = document.createElement("div");
+
     date.innerHTML = `${weekDays[DateObject.getDay()]}, ${months[DateObject.getMonth()]} ${DateObject.getDate()}   ${DateObject.getFullYear()}`;
     time.innerHTML = `${DateObject.getHours()} : ${DateObject.getMinutes()} : ${DateObject.getSeconds()}`;
+
+    clockFace.appendChild(date);
+    clockFace.appendChild(time);
 }
 
-let clockFace = setInterval(() => renderDefaultClock(), 500);
+const renderStopwatch = () => {
+    stopwatchButton.hidden = true; 
+    watchButton.hidden = false;    
+    timerButton.hidden = false;
+    clearInterval(intializeClock);
+    watchFace.innerHTML = "";
+}
+
+
+let intializeClock = setInterval(() => renderClock(watchFace), 500);
+
+stopwatchButton.addEventListener('click',() => renderStopwatch())
