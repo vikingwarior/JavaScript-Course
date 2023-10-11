51cb818bfbeb77bda6d0c85506741e5be683253f
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-10-12 00:28:08 +0530

    Significance and working of Finally

--- Harsh/36-Error-Object/script.js
+++ Harsh/36-Error-Object/script.js
@@ -6,5 +6,10 @@ try {
 } catch (e) {
     console.log(`Printing Error message using e.message : ${e.message}`);
     console.log(`Printing Error name using e.name : ${e.name}`);
-    console.log(`Printing Error stack using e.stack : ${e.stack}`);
+    console.log(`Printing Error stack using e.stack :\n${e.stack}`);
+    throw MediaError(`Error in catch block ${MediaError.message}`);
+}
+// The finally keyword ensures that the code inside it always executes, irrespective of the situation. If it is written in a function. This block will run even if the return before it or there is an error in catch block The JS engine ensures that the code inside finally should be executed before the function is returned or error is thrown in the respective conditions
+finally{
+    console.log(`Finally block is executed`);
 }
