af161f261e04496e4c6b4973367e084ce685238d
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-11-01 23:41:38 +0530

    modified:   Harsh/39-fetch-API/script.js

--- Harsh/39-fetch-API/script.js
+++ Harsh/39-fetch-API/script.js
@@ -2,7 +2,7 @@
  * fetch() -> starts the process of fetching a resource from the network, returning a promise which is fulfilled once the response is available.
  * 
  * NOTE ->
- * The desired value is often encapsulated inside the response body that contains the status of the request. To extract the desired value, response.json() Needs to be fetched 
+ * The desired value is often encapsulated inside the response body that contains the status of the request. To extract the desired value, response.json() Needs to be fetched.
  * Retriving the promise from the fetch() is 2 stage process
  */
 
