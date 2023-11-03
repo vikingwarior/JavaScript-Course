002764b9480fcb268aac1ea355dc06243d999528
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-11-03 23:27:08 +0530

    modified:   Harsh/39-fetch-API/script.js

--- Harsh/39-fetch-API/script.js
+++ Harsh/39-fetch-API/script.js
@@ -5,6 +5,8 @@
  * The desired value is often encapsulated inside the response body that contains the status of the request. To extract the desired value, response.json() Needs to be fetched.
  * Retriving the promise from the fetch() is 2 stage process.
  * Following is an example of a GET Request ->
+ * Get Request -> When data is sent by appending it to a URL
+ * Limitation -> Cannot send large data due to URL limitations
  */
 
 let fetchAPIGet = async () => {
