d3bb370722b94fc85525d5d1997f1ecdc08d9d19
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-11-14 01:40:56 +0530

    Basic behavior

--- code-files/43-Exercise-6/script.js
+++ code-files/43-Exercise-6/script.js
@@ -1,52 +1,76 @@
-let toDoTasks = [];
-let doneTasks = [];
+let toDoTasks = [`Grocery`, `Dusting`];
+let doneTasks = [`Walking the pets`, `Laundry`];
 
-const createTaskBtn = () => {
-    let btn = document.createElement('button');
-    btn.classList = `btn btn-outline-primary rounded-circle border btn-outline-info border-0 ms-auto`;
-    btn.innerHTML = `+`;
-    return btn;
+const createBtn = (btnLabel) => {
+  let btn = document.createElement("button");
+  btn.classList = `btn btn-outline-primary rounded-circle border btn-outline-info border-0 ms-auto`;
+  btn.innerHTML = btnLabel;
+  return btn;
 };
 
-const createListItem = (taskName, taskType, addButton=false) => {
+const createListItem = (taskName=null, taskType=null, addButton = false) => {
+    let listItemContainer = document.createElement(`div`);
+    listItemContainer.classList = `listItemContainer`;
+
+    let listItem = document.createElement(`li`);
+    listItem.classList = `list-group-item d-flex align-items-center`;
+
+  if (taskName != null) {
     let checkBox = document.createElement(`input`);
     checkBox.type = `checkbox`;
     checkBox.classList = `form-check-input me-1`;
 
     let taskLabel = document.createElement(`label`);
     taskLabel.classList = `form-check-label`;
-    taskLabel.innerHTML = `<s>${taskName}</s>` ? taskName : taskType == `completed`;
+    taskLabel.innerHTML = `<s>${taskName}</s>`
+      ? taskName
+      : taskType == `completed`;
 
-    let listItemContainer = document.createElement(`div`);
-    listItemContainer.classList = `listItemContainer`;
     listItemContainer.appendChild(checkBox);
     listItemContainer.appendChild(taskLabel);
-
-    let listItem = document.createElement(`li`);
-    listItem.classList = `list-group-item d-flex align-items-center`;
     listItem.appendChild(listItemContainer);
 
-    if (addButton) listItem.appendChild(createTaskBtn());
-
-    return listItem;
+    if (addButton) {
+      let newTaskBtn = createBtn(`+`);
+      newTaskBtn.addEventListener(`click`, () => {
+        let lastListItem = document.getElementById(`last`);
+        lastListItem.removeAttribute(`id`);
+        lastListItem.insertAdjacentElement(`afterend`, createListItem());
+      });  
+      listItem.appendChild(createBtn(`+`));
+      listItem.id = `last`;
+    }
+  } else {
+    listItemContainer.innerHTML = `<input class="form-control form-control border border-info border-0" type="text"
+    placeholder="Add your Task">`;
+    listItem.appendChild(listItemContainer);
+    listItem.appendChild(createBtn(`ü`))
+  }
+  
+  return listItem;
 };
 
 const isLastEntry = (arr, value) => {
-
-    if (arr.length > 0) return value === arr[arr.length - 1];
-    return false;
-}
-
-const renderTasksTable = (tasks, listType, containerReference) => {
-    let listContainer = document.createElement(`ul`);
-    listContainer.classList = `list-group`;
-
-    for (const taskName in tasks) {
-        listContainer.appendChild(createListItem(taskName, listType, false));
-
-        if (isLastEntry(tasks, taskName)) listContainer.appendChild(createListItem(taskName, listType, true));
-    }
-
-    containerReference.appendChild(listContainer);
-
+  if (arr.length > 0) return value === arr[arr.length - 1];
+  return false;
 };
+
+const createTasksTable = (tasks, listType) => {
+  let listContainer = document.createElement(`ul`);
+  listContainer.classList = `list-group`;
+
+  for (let taskName of tasks) {
+    if (isLastEntry(tasks, taskName))
+      listContainer.appendChild(createListItem(taskName, listType, true));
+    else listContainer.appendChild(createListItem(taskName, listType, false));
+  }
+
+  return listContainer;
+};
+
+let toDolistContainer = document.getElementsByClassName(`toDoList`)[0];
+let completedListContainer =
+  document.getElementsByClassName(`completedList`)[0];
+
+toDolistContainer.appendChild(createTasksTable(toDoTasks, `toDo`));
+toDolistContainer.appendChild(createListItem());
