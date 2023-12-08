7c0d88a01daa8473ab5f5c0c051148fae794a5a4
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-12-09 01:40:36 +0530

    Resolved -> 1. Irregular list entry issue 2. Button Behavaior
    Added ->
    1. Dummy code for faster testing

--- code-files/43-Exercise-6/script.js
+++ code-files/43-Exercise-6/script.js
@@ -16,9 +16,14 @@ const createTasksTable = (tasks, completed = false) => {
     let listItem = createListItem(taskName, completed, lastEntryFlag);
 
     let checkbox = listItem.querySelector('input[type="checkbox"]');
-    checkbox.addEventListener(`change`, () => {
-      if (checkbox.checked) addTaskToList(toDoTasks, doneTasks, taskName);
-      else addTaskToList(doneTasks, toDoTasks, taskName);
+    checkbox.addEventListener('change', () => {
+      if (checkbox.checked) {
+        addTaskToList(doneTasks, toDoTasks, taskName);
+        removeElementFromDOM(listItem);
+      } else {
+        addTaskToList(toDoTasks, doneTasks, taskName);
+        removeElementFromDOM(listItem);
+      }
     });
 
     listContainer.appendChild(listItem);
@@ -126,8 +131,80 @@ const isLastEntry = (arr, value) => {
   return false;
 };
 
-let toDolistContainer = document.getElementsByClassName(`toDoList`)[0];
-let completedListContainer = document.getElementsByClassName(`completedList`)[0];
+const removeElementFromDOM = (elementToBeRemoved) => {
+  document.removeChild(elementToBeRemoved);
+};
+
+let toDolistContainer = document.getElementsByClassName(`listContainer`)[0];
 
 let tasksTable = createTasksTable(toDoTasks);
 toDolistContainer.appendChild(tasksTable);
+
+// Boilerplate code for testing
+
+// HTML:
+// <!DOCTYPE html>
+// <html data-bs-theme="dark">
+
+// <head>
+//   <meta charset="utf-8">
+//   <meta name="viewport" content="width=device-width">
+//   <title>template</title>
+//   <link href="style.css" rel="stylesheet" type="text/css" />
+//   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+//     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+// </head>
+
+// <body>
+//    <div class="tabs d-flex flex-column justify-content-center align-items-center">
+//     <ul class="nav nav-pills my-4 ">
+//       <li class="toDo nav-item">
+//         <a class="nav-link active" href="#">To Do</a>
+//       </li>
+//       <li class="completed nav-item">
+//         <a class="nav-link" href="#">Completed</a>
+//       </li>
+//     </ul>
+//   </div>
+//   <div class="toDoList d-flex flex-column justify-content-center align-items-center">
+//   <ul class="list-group"><li class="list-group-item d-flex align-items-center"><div class="listItemContainer"><input type="checkbox" class="form-check-input me-1"><label class="form-check-label">Grocery</label></div></li><li class="list-group-item d-flex align-items-center" id="last"><div class="listItemContainer"><input type="checkbox" class="form-check-input me-1"><label class="form-check-label">Dusting</label></div><button class="btn btn-outline-primary rounded-circle border btn-outline-info border-0 ms-auto">+</button></li></ul></div>
+//   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+//     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+//     crossorigin="anonymous"></script>
+// </body>
+
+// </html>
+
+// JS:
+
+// let toDoTasks = [`Grocery`, `Dusting`];
+// let doneTasks = [`Walking the pets`, `Laundry`];
+
+// let ulElems = document.getElementsByClassName('list-group-item d-flex align-items-center');
+
+// for (let listItem of ulElems) {
+//   let checkbox = listItem.querySelector('input[type="checkbox"]');
+//   let taskName = listItem.querySelector('.form-check-label').innerText;
+  
+//    checkbox.addEventListener('change', () => {
+//     if (checkbox.checked) {
+//       addTaskToList(doneTasks, toDoTasks, taskName);
+//     } else {
+//       addTaskToList(toDoTasks, doneTasks, taskName);
+//     }
+//   });
+// }
+
+// const addTaskToList = (listToAddTaskTo, listToRemoveTaskFrom, taskName) => {
+//   const index = listToRemoveTaskFrom.indexOf(taskName);
+
+//   if (index !== -1) {
+//     listToRemoveTaskFrom.splice(index, 1);
+//     listToAddTaskTo.push(taskName);
+
+//     console.log(toDoTasks);
+//     console.log(doneTasks);
+//     console.log(taskName);
+//   }
+// };
+
