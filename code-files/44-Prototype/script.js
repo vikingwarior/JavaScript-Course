ef82d8ed24a9aed7b2af038db6a95ccbc350a7d1
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-12-14 23:23:39 +0530

    Added definition for prototype

---
+++ code-files/44-Prototype/script.js
@@ -0,0 +1,4 @@
+/**
+ Prototype -> Consider this a blueprint of a JS object that has a set of properties defined. 
+ - When we try to read a property of an object that is missing, it is by default taken from prototype of that object.
+ */
