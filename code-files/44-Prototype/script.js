809bef56d82134eff38034bea32ef0c91084a9c7
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-12-15 13:45:39 +0530

    Added rules for using OBJECT_NAME.__proto__

--- code-files/44-Prototype/script.js
+++ code-files/44-Prototype/script.js
@@ -1,4 +1,6 @@
 /**
  Prototype -> Consider this a blueprint of a JS object that has a set of properties defined. 
  - When we try to read a property of an object that is missing, it is by default taken from prototype of that object.
+ - If you want modify/mutate the [[Prototype]] of an object you can use OBJECT_NAME.__proto__ to access the Prototype.
+ - An object can hav "Multiple level" of prototypes.
  */
