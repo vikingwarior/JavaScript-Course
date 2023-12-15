6ed7250912c537ed6367c6d1ec8250c594fa6832
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-12-15 14:38:59 +0530

    Added example code

--- code-files/44-Prototype/script.js
+++ code-files/44-Prototype/script.js
@@ -1,6 +1,27 @@
 /**
  Prototype -> Consider this a blueprint of a JS object that has a set of properties defined. 
  - When we try to read a property of an object that is missing, it is by default taken from prototype of that object.
- - If you want modify/mutate the [[Prototype]] of an object you can use OBJECT_NAME.__proto__ to access the Prototype.
+ - If you want modify/add to/mutate the [[Prototype]] of an object you can use OBJECT_NAME.__proto__ to access the Prototype.
  - An object can hav "Multiple level" of prototypes.
  */
+
+let organiztion = {
+    oName : `PTC`
+};
+
+let greetProto = {
+  greet: () => {
+    console.log(`Hello World!`);
+  },
+};
+
+greetProto.__proto__ = organiztion;
+
+console.log(greetProto);
+
+let employee = { name: `Harsh`, DOB: `23 Sept 2000` };
+
+employee.__proto__ = greetProto;
+
+employee.greet();
+console.log(employee.oName);
