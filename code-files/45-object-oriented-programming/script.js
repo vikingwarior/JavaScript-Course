c89f81af53ac24fb36a8898d7db721afa042ec87
Harsh Paradkar <contact.harshparadkar@gmail.com>
2023-12-24 15:45:11 +0530

    Getters and Setters

--- code-files/45-object-oriented-programming/script.js
+++ code-files/45-object-oriented-programming/script.js
@@ -5,10 +5,12 @@ class BaseClass {
 
   test() {
     console.log(`Base Class Test Method`);
-  };
+  }
 
   static greet() {
-    console.log('This is a static function directly invoked from BaseClass.greet()');
+    console.log(
+      "This is a static function directly invoked from BaseClass.greet()"
+    );
   }
 }
 
@@ -28,9 +30,40 @@ class DerivedClass extends BaseClass {
 let derObj = new DerivedClass(`23/09/2000`);
 derObj.invokeTest();
 
-
 // Static method -> A method of class that can be directly called on the class (i.e.Without creating its object is called static method).
 BaseClass.greet();
 
 // If you pass a static function from the object, it will throw an error(because it is not present in the prototype of the object.)
 // derObj.greet();
+
+// getters and setter -> Special types of JS functions, that define how to display or modify the value of an object
+
+class GetterAndSetterClass {
+  constructor(param) {
+    this._param = param;
+  }
+
+  get param() {
+    return this._param;
+  }
+
+  set param(param) {
+    this._param = param;
+  }
+
+  testSet(param) {
+    this._param = param;
+  }
+
+  testGet() {
+    return this._param;
+  }
+}
+
+const sampleObj = new GetterAndSetterClass(58);
+
+console.log(sampleObj.param);
+
+sampleObj.param = `lol`;
+
+console.log(sampleObj.param);
