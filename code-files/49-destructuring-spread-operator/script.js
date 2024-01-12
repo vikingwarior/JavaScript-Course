68f0552cfa1ea2c543a96503a82c5373e508e382
Harsh Paradkar <73878892+VikingWarior@users.noreply.github.com>
2024-01-12 20:04:19 +0530

    Output structuring

--- code-files/49-destructuring-spread-operator/script.js
+++ code-files/49-destructuring-spread-operator/script.js
@@ -3,10 +3,16 @@
 let arr = [1, 2, 3, 4, 5];
 
 let [a, b] = arr // a -> 1, b -> 2
-console.log(`a:${a} b:${b}`);
+console.log(`a:${a}\nb:${b}`);
 
 let [,c,,d] = arr // c -> 2, b -> 4
-console.log(`c:${c} d:${d}`);
+console.log(`c:${c}\nd:${d}`);
 
 let [e,,,...subArr] = arr // e -> 1 subArr -> [4,5]
-console.log(`e:${e} subArr:[${subArr}]`);
+console.log(`e:${e}\nsubArr:[${subArr}]`);
+
+//Spread -> Syntax that allows to extract arguments from an iterable structure and map them accordingly
+
+let obj = {...subArr};
+console.log(obj);
+
