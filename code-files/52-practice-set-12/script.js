af9a6d9bd0c8b8f286e534c7bcb20a7b7b034ccf
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-01-17 00:29:26 +0530

    Q2 Solution

--- code-files/52-practice-set-12/script.js
+++ code-files/52-practice-set-12/script.js
@@ -13,3 +13,15 @@ async function printMessage(message) {
     await printMessage("Hello");
     await printMessage("World");
 })();
+
+
+// Q2 -> Finding Average of Numbers in an array using spread syntax
+
+let arr = [11, 62, 37, 14, 15];
+
+function getAvg(...nums) {
+    let sum = nums.reduce((a, b) => a + b, 0);
+    return Math.floor(sum / arr.length);
+}
+
+console.log(getAvg(...arr));
